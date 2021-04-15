1e8136b09e5a2f109e1e75f0128eb1f9e5d5184d
Abhishekpandey11 <thepeopleschamp12345@gmail.com>
2021-04-15 23:11:21 +0530

    upploading assignment 2

---
+++ Personel/abhipandey/python/assignment_2/pnz.py
@@ -0,0 +1,8 @@
+a = int(input("enter the number : "))
+if a < 0 :
+    print("the number is negative ")
+elif a > 0 :
+    print("the number is positive ")
+else :
+    print("the number is equal to zero ")
+    
---
+++ Personel/abhipandey/python/assignment_2/str.py
@@ -0,0 +1,7 @@
+d = input("enter the character : ")
+if d.isdigit() == True :
+    print("it is a digit ")
+elif d.isalpha() ==True :
+    print("it is al[phabet ")
+else :
+    print("it is a special character ")
