c9afba2af08185a40050baf9aa8a84d5518a55e0
Satyanshuk <itsmesatyanshu@gmail.com>
2021-04-15 21:29:52 +0530

    uploading assignmnt 2

---
+++ Personel/satyanshu/Python/assignmt_2/alpha_digit_special.py
@@ -0,0 +1,8 @@
+#digit,alphabet or special character
+data = input("Enter a Character : ")
+if data.isdigit() == True :
+    print("Enetered Character is Digit")
+elif data.isalpha() == True :
+    print("Entered Character is Alphabet")
+else :
+    print("Entered Character is Special Character")
