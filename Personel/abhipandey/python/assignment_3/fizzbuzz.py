6034cd77dc6b4a1f79207c1e1e28d5f6f9c29ccb
Abhishekpandey11 <thepeopleschamp12345@gmail.com>
2021-04-21 22:37:40 +0530

    correction done

--- Personel/abhipandey/python/assignment_3/fizzbuzz.py
+++ Personel/abhipandey/python/assignment_3/fizzbuzz.py
@@ -1,8 +1,9 @@
-for a in range (51):
+for a in range (1,51):
     if a %3==0 and a %5 ==0:
         print("fizbuzz")
     elif a %3 ==0:
         print("fizz")
     elif a %5 ==0:
         print("buzz")
-    print(a)
+    else:
+        print(a)
