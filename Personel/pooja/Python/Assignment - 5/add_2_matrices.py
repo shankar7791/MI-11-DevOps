b652d6a7e5b4604f9c3051f66609afa528739afe
Pooja Paralkar <poojanandura@gmail.com>
2021-04-23 16:07:33 +0530

    uploading python Assignment - 5

---
+++ Personel/pooja/Python/Assignment - 5/add_2_matrices.py
@@ -0,0 +1,17 @@
+x =[[10,7,3],
+    [9,5,6],
+    [6,8,9]]
+
+y =[[5,8,1],
+    [6,7,3],
+    [4,9,5]]
+
+result =[[0,0,0],
+          [0,0,0],
+          [0,0,0]]
+
+for i in range (len(x)):
+    for j in range (len(x[0])):
+        result [i][j] = x[i][j] + y[i][j] 
+for r in result:
+    print(r)
---
+++ Personel/pooja/Python/Assignment - 5/len_of_string.py
@@ -0,0 +1,8 @@
+def findLen(str):
+    count = 0
+    for i in str :
+        count += 1
+    return count
+
+str = "Python"
+print("Length of a String :",findLen(str))
