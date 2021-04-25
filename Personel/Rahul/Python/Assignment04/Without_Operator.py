af9aa0d653035c3fc718d9a91757c2c1ee08c9e1
Rahulgupta19980 <jerrygupta1998@gmail.com>
2021-04-25 19:09:15 +0530

    Uploading Assignment 04 and 05

---
+++ Personel/Rahul/Python/Assignment04/Without_Operator.py
@@ -0,0 +1,4 @@
+import operator
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+print("Addition Of Two Number Without Using Operator Is :",operator.add(a,b))
---
+++ Personel/Rahul/Python/Assignment04/thirdsideoftriangle.py
@@ -0,0 +1,4 @@
+s1 = int(input("Triangle of first side : "))
+s2 = int(input("Triangle of second side : "))
+s3 = ((s1 ** 2) + (s2 ** 2)) ** 0.5
+print("The Third side of Right angled Triangle is :",s3)
