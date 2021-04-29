1a94f1e9fbe0cc0c308929c85365dabdcb10b63b
Pooja Paralkar <poojanandura@gmail.com>
2021-04-29 22:56:03 +0530

    uploading Python Assignment - 8

---
+++ Personel/pooja/Python/Assignment - 8/factorial_num.py
@@ -0,0 +1,18 @@
+# Factorial of a number using recursion
+
+def recur_factorial(n):
+   if n == 1:
+       return n
+   else:
+       return n*recur_factorial(n-1)
+
+num = int(input("Enter a number : "))
+
+if num < 0:
+   print("Enter positive number")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   print("The factorial of", num, "is", recur_factorial(num))
+
+
