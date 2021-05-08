ac3d85b046641f6d4f0518bf74f083b9d3d353bd
Akshay5596 <akshay.patil5596@gmail.com>
2021-05-08 10:42:40 +0530

    my asingment 6

---
+++ Personel/Akshay/python/Assingment_6/05_pangram.py
@@ -0,0 +1,9 @@
+# Write a Python function to check whether a string is a pangram or not
+l = [95, 28, 47, 31, 74, 41, 84, 40, 71, 84]
+def even(list) :
+    print ("Even number is :")
+    for i in list :
+        if (i % 2 == 0) :
+            print (i ,end= " ")
+    print()
+even(l)
