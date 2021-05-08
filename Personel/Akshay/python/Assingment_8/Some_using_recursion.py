4a48722888d555cb2d57076bb10c50d40d70086a
Akshay5596 <akshay.patil5596@gmail.com>
2021-05-08 10:45:50 +0530

    assingment no  8

---
+++ Personel/Akshay/python/Assingment_8/Some_using_recursion.py
@@ -0,0 +1,14 @@
+
+#dum using recursion
+def recur(n) :
+    if n <= 1 :
+        return n
+    else :
+        return n + recur(n-1)
+
+val = int(input("Enter a Number : "))
+
+if val < 0 :
+    print("Enter a Positive Number.")
+else :
+    print(f"Sum of sequence of number upto {val} is {recur(val)}")
