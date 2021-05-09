dd5427c2166af08bf7f8f2f8cc78552d8c41e041
Abhishekpandey11 <thepeopleschamp12345@gmail.com>
2021-05-09 21:03:18 +0530

    adding up assignment 12

---
+++ Personel/abhipandey/python/assignment_12/angram.py
@@ -0,0 +1,22 @@
+def Anagram(input):
+
+	dict = {}
+
+	for strVal in input:
+
+		key = ''.join(sorted(strVal))
+
+		if key in dict.keys():
+			dict[key].append(strVal)
+		else:
+			dict[key] = []
+			dict[key].append(strVal)
+
+	output = ""
+	for key,value in dict.items():
+		output = output + ' '.join(value) + ' '
+
+	return output
+
+input=['cat', 'tac' , 'sat' , ]
+print (Anagram(input))
---
+++ Personel/abhipandey/python/assignment_12/tup_to_dict.py
@@ -0,0 +1,7 @@
+def Convert(tupl, dic):
+    dic = dict(tupl)
+    return dic
+
+tupl = [("tony", 30), ("clark", 31), ("natasha", 30), ("banner", 35), ("steve", 101), ("thor", 1500)]
+dictionary = {}
+print (Convert(tupl, dictionary)) 
