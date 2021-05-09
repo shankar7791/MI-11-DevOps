a907359816fd174494787a75ab9a45b9e0010221
Pooja Paralkar <poojanandura@gmail.com>
2021-05-09 19:55:09 +0530

    uploading Python Assignment - 12

---
+++ Personel/pooja/Python/Assignment - 12/list_tuples into dict.py
@@ -0,0 +1,7 @@
+def Convert(tup, di):
+    di = dict(tup)
+    return di
+      
+tups = [("Pooja", 10), ("Megha", 12), ("Savita", 28), ("Mahima", 20), ("Pratiksha", 25), ("Swara", 30)]
+dictionary = {}
+print (Convert(tups, dictionary))
