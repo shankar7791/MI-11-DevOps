ae39a744ff7801ec83caaf2979c18fbf9a9c54b1
Satyanshuk <itsmesatyanshu@gmail.com>
2021-05-19 16:54:20 +0530

    assignmnt 13

---
+++ Personel/satyanshu/Python/assignmnt 13/array.py
@@ -0,0 +1,29 @@
+
+def same(col, pat):
+    if len(col) != len(pat):
+        return False
+    sdict = {}
+    pset = set()
+    sset = set()
+    for i in range(len(pat)):
+        pset.add(pat[i])
+        sset.add(col[i])
+        if pat[i] not in sdict.keys():
+            sdict[pat[i]] = []
+
+        keys = sdict[pat[i]]
+        keys.append(col[i])
+        sdict[pat[i]] = keys
+
+    if len(pset) != len(sset):
+        return False
+    
+    for val in sdict.values():
+        for i in range(len(val) - 1):
+            if val[i] != val[i+1]:
+                return False
+
+    return True
+
+print("(color1,pat) = "),print(same(["red","green","green"],["a","b","b"]))
+print("(strings,color2) = "),print(same(["red","green","greennn"],["a","b","b"]))
---
+++ Personel/satyanshu/Python/assignmnt 13/largest.py
@@ -0,0 +1,3 @@
+
+lis = [int(item) for item in input("Enter the numbers in the list : ").split()]
+print("largest number from the list is : ", max(lis))
