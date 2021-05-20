9a59cc9af3e6a2f608d03ac022a7847a34d44633
Pooja Paralkar <poojanandura@gmail.com>
2021-05-20 23:36:01 +0530

    Adding Selection sort and Bubble sort

---
+++ Personel/pooja/Python/Assignment - 16/bubble_sort.py
@@ -0,0 +1,19 @@
+def bubbleSort(arr):
+	n = len(arr)
+
+	for i in range(n-1):
+	
+		for j in range(0, n-i-1):
+
+		
+			if arr[j] > arr[j+1] :
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+
+
+arr = [25, 30, 5, 100, 60, 55, 20, 1]
+
+bubbleSort(arr)
+
+print ("Sorted array is:")
+for i in range(len(arr)):
+	print ("%d" %arr[i])
