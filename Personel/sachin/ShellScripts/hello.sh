dd0668f164e9df955b933247bedfb49031909e11
d1v1debyzero <srai47309@gmail.com>
2021-07-09 21:11:55 +0530

    uploading assessment

---
+++ Personel/sachin/ShellScripts/hello.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/bash
+echo "hello! this is my first script ...."
+echo This is the location of bash : $BASH
+echo Home location : $HOME
+
---
+++ Personel/sachin/ShellScripts/name_age.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/bash
+#age=22
+#name=sachin
+#echo "My name is $name & age is $age"
+
+
+# read -p 'Enter the name : ' pass 
+#read -sp (-sp is use to hide password)
+
+
+
+
+read -p 'Enter the username : ' username 
+read -sp 'Enter the password : ' password
+echo""
+echo "username is $username & password is $password"
+
+
+
+#$REPLY variable is used to store clipboard data 
+
